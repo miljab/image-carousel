5959a4efa98d6254c46a5f21ac215e8326b33ea2
miljab <jablonskimilosz1337@gmail.com>
2024-09-11 21:47:51 +0200

    Add slider nav bar and interval

--- src/image_carousel.js
+++ src/image_carousel.js
@@ -1,24 +1,109 @@
-export function imageCarousel(slider, previousButton, nextButton, imgWidth) {
+export function imageCarousel(
+  slider,
+  previousButton,
+  nextButton,
+  imgWidth,
+  nav,
+) {
   const sliderWidth = parseInt(slider.scrollWidth);
   slider.style.right = 0;
 
+  const numberOfImages = sliderWidth / imgWidth;
+  const navButtons = [];
+
+  for (let i = 0; i < numberOfImages; i++) {
+    const navButton = document.createElement("button");
+    navButtons.push(navButton);
+    navButton.classList.add("nav-button");
+
+    if (i == 0) navButton.style.backgroundColor = "black";
+    nav.appendChild(navButton);
+  }
+
+  let sliderInterval = startInterval(
+    slider,
+    sliderWidth,
+    imgWidth,
+    navButtons,
+    numberOfImages,
+  );
+
+  for (let i = 0; i < numberOfImages; i++) {
+    navButtons[i].addEventListener("click", () => {
+      clearInterval(sliderInterval);
+      slider.style.right = `${imgWidth * i}px`;
+      navUpdate(navButtons, i, numberOfImages);
+      sliderInterval = startInterval(
+        slider,
+        sliderWidth,
+        imgWidth,
+        navButtons,
+        numberOfImages,
+      );
+    });
+  }
+
   nextButton.addEventListener("click", () => {
-    nextImg(slider, sliderWidth, imgWidth);
+    clearInterval(sliderInterval);
+    nextImg(slider, sliderWidth, imgWidth, navButtons, numberOfImages);
+    sliderInterval = startInterval(
+      slider,
+      sliderWidth,
+      imgWidth,
+      navButtons,
+      numberOfImages,
+    );
   });
 
   previousButton.addEventListener("click", () => {
-    previousImg(slider, sliderWidth, imgWidth);
+    clearInterval(sliderInterval);
+    previousImg(slider, imgWidth, navButtons, numberOfImages);
+    sliderInterval = startInterval(
+      slider,
+      sliderWidth,
+      imgWidth,
+      navButtons,
+      numberOfImages,
+    );
   });
 }
 
-function nextImg(slider, sliderWidth, imgWidth) {
+function nextImg(slider, sliderWidth, imgWidth, navButtons, n) {
   if (parseInt(slider.style.right) < sliderWidth - imgWidth) {
     slider.style.right = `${parseInt(slider.style.right) + imgWidth}px`;
+    let index = parseInt(slider.style.right) / imgWidth;
+    navUpdate(navButtons, index, n);
   }
 }
 
-function previousImg(slider, sliderWidth, imgWidth) {
+function previousImg(slider, imgWidth, navButtons, n) {
   if (parseInt(slider.style.right) > 0) {
     slider.style.right = `${parseInt(slider.style.right) - imgWidth}px`;
+    let index = parseInt(slider.style.right) / imgWidth;
+    navUpdate(navButtons, index, n);
   }
+}
+
+function navUpdate(buttons, index, n) {
+  for (let j = 0; j < n; j++) {
+    if (j != index) buttons[j].style.backgroundColor = "white";
+    else buttons[index].style.backgroundColor = "black";
+  }
+}
+
+function startInterval(
+  slider,
+  sliderWidth,
+  imgWidth,
+  navButtons,
+  numberOfImages,
+) {
+  return setInterval(() => {
+    if (parseInt(slider.style.right) == sliderWidth - imgWidth) {
+      slider.style.right = 0;
+      navUpdate(navButtons, 0, numberOfImages);
+    } else {
+      nextImg(slider, sliderWidth, imgWidth, navButtons, numberOfImages);
+    }
+  }, 5000);
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import "./styles.css";
 const slider = document.querySelector(".slider");
 const previousButton = document.querySelector("#previous-button");
 const nextButton = document.querySelector("#next-button");
+const nav = document.querySelector(".slider-nav");
 
-imageCarousel(slider, previousButton, nextButton, 500);
+imageCarousel(slider, previousButton, nextButton, 500, nav);
